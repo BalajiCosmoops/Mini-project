ead589cb108fae46a6abb5d165f65ebf64609183
Balaji-Dev <balaji01122000@gmail.com>
2025-07-10 23:14:06 +0530

    10 july status

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const menuToggle = document.getElementById("menu-toggle");
+const menuToggle = document.getElementById("bar-1");
 const dropdownMenu = document.getElementById("dropdown");
+const sidebar=document.getElementById("sidebar-1")
 
 menuToggle.addEventListener("click", function () {
   if (dropdownMenu.style.display === "none" || dropdownMenu.style.display === "") {
     dropdownMenu.style.display = "block";
+    sidebar.style.backgroundColor="#635c5c";
   } else {
     dropdownMenu.style.display = "none";
+    sidebar.style.backgroundColor="transparent";
   }
 });
